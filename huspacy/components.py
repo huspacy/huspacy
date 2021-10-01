819f40ce326f37efab630200287545cca29bcb0f
Gyorgy Orosz <gyorgy@orosz.link>
2021-10-01 16:15:32 +0200

    Added the lemmy model to assemble.cfg

--- huspacy/components.py
+++ huspacy/components.py
@@ -1,13 +1,15 @@
-# coding: utf8
-from __future__ import unicode_literals
-
 import re
+from pathlib import Path
+from typing import Union
 
+from lemmy import Lemmatizer
 from spacy.language import Language
+from spacy.tokens import Token
 from spacy.tokens.doc import Doc
 
 
-class HunSentencizer(object):
+class HunSentencizer:
+    # noinspection PyUnusedLocal
     @staticmethod
     @Language.factory("hun_sentencizer")
     def create(nlp: Language, name: str) -> "HunSentencizer":
@@ -24,3 +26,28 @@ class HunSentencizer(object):
                 if next_token.text not in self._quote_or_bracket:
                     next_token.is_sent_start = True
         return doc
+
+
+class HunLemmatizer:
+    # noinspection PyUnusedLocal
+    @staticmethod
+    @Language.factory("hun_lemmy")
+    def create(nlp: Language, name: str, model_path: Union[str, Path]) -> "HunLemmatizer":
+        return HunLemmatizer(model_path)
+
+    def __init__(self, model_path: Union[str, Path]):
+        self._lemmy: Lemmatizer = Lemmatizer.from_disk(model_path)
+
+    def __call__(self, doc: Doc) -> Doc:
+        token: Token
+        for token in doc:
+            token.lemma_ = self._lemmy.lemmatize(token.tag_, token.text)
+        return doc
+
+    # noinspection PyUnusedLocal
+    def to_disk(self, path, exclude=tuple()):
+        self._lemmy.to_disk(path)
+
+    # noinspection PyUnusedLocal
+    def from_disk(self, path, exclude=tuple()):
+        self._lemmy.from_disk(path)
