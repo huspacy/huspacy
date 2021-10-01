8a374ac5962d62831baeb11ddcd94c9da75bcb8d
Gyorgy Orosz <gyorgy@orosz.link>
2021-10-01 15:40:38 +0200

    Added tests for the sentencizer.

--- huspacy/components.py
+++ huspacy/components.py
@@ -4,15 +4,20 @@ from __future__ import unicode_literals
 import re
 
 from spacy.language import Language
+from spacy.tokens.doc import Doc
 
 
-@Language.component("hun_sentencizer")
 class HunSentencizer(object):
+    @staticmethod
+    @Language.factory("hun_sentencizer")
+    def create(nlp: Language, name: str) -> "HunSentencizer":
+        return HunSentencizer()
+
     def __init__(self):
         self._boundary_punct_pattern = re.compile(r"^((\.)|[\?!]+)$")
         self._quote_or_bracket = {'"', ")"}
 
-    def __call__(self, doc):
+    def __call__(self, doc: Doc) -> Doc:
         for token in doc[:-1]:
             if self._boundary_punct_pattern.match(token.text):
                 next_token = doc[token.i + 1]
