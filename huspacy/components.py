dec5a27843743ca9341207d5de48f05c1222b782
Gyorgy Orosz <gyorgy@orosz.link>
2021-10-28 14:08:04 +0200

    Updated lemmy to v3.2

--- huspacy/components.py
+++ huspacy/components.py
@@ -1,11 +1,11 @@
 import re
-from pathlib import Path
-from typing import Union
 
-from lemmy import Lemmatizer
 from spacy.language import Language
 from spacy.tokens import Token
 from spacy.tokens.doc import Doc
+
+# noinspection PyUnresolvedReferences
+import lemmy
 
 
 class HunSentencizer:
@@ -32,28 +32,3 @@ class HunSentencizer:
                 elif len(token.whitespace_) > 0:
                     next_token.is_sent_start = True
         return doc
-
-
-class HunLemmatizer:
-    # noinspection PyUnusedLocal
-    @staticmethod
-    @Language.factory("hun_lemmy")
-    def create(nlp: Language, name: str, model_path: Union[str, Path]) -> "HunLemmatizer":
-        return HunLemmatizer(model_path)
-
-    def __init__(self, model_path: Union[str, Path]):
-        self._lemmy: Lemmatizer = Lemmatizer.from_disk(model_path)
-
-    def __call__(self, doc: Doc) -> Doc:
-        token: Token
-        for token in doc:
-            token.lemma_ = self._lemmy.lemmatize(token.tag_, token.text)
-        return doc
-
-    # noinspection PyUnusedLocal
-    def to_disk(self, path, exclude=tuple()):
-        self._lemmy.to_disk(path)
-
-    # noinspection PyUnusedLocal
-    def from_disk(self, path, exclude=tuple()):
-        self._lemmy.from_disk(path)
