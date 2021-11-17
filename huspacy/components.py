35ea64663917314df8b5949b65408183e3ec22cd
Gyorgy Orosz <gyorgy@orosz.link>
2021-11-17 22:48:17 +0100

    Improved sentencizer

--- huspacy/components.py
+++ huspacy/components.py
@@ -1,11 +1,9 @@
 import re
+from typing import Optional
 
 from spacy.language import Language
 from spacy.tokens import Token
 from spacy.tokens.doc import Doc
-
-# noinspection PyUnresolvedReferences
-import lemmy
 
 
 class HunSentencizer:
@@ -16,19 +14,35 @@ class HunSentencizer:
         return HunSentencizer()
 
     def __init__(self):
-        self._boundary_punct_pattern = re.compile(r"^((\.)|[\?!]+)$")
+        self._boundary_punct_pattern = re.compile(r"^([.\?!]+)$")
         self._quote_or_bracket = {'"', ")"}
+        self._mdash = "—"
 
     def __call__(self, doc: Doc) -> Doc:
         doc[0].is_sent_start = True
 
+        token: Token
         for token in doc[:-1]:
-            if self._boundary_punct_pattern.match(token.text):
-                next_token: Token = doc[token.i + 1]
-                if next_token.text[0].islower():
+            next_token: Token = doc[token.i + 1]
+            has_next2: bool = len(doc) > token.i + 2
+            next2_token: Optional[Token]
+            if has_next2:
+                next2_token = doc[token.i + 2]
+            else:
+                next2_token = None
+
+            if self._boundary_punct_pattern.match(token.text) is not None:
+                if next_token.text == self._mdash:
+                    if has_next2 and next2_token.text[0].isupper():
+                        next_token.is_sent_start = True
+                elif next_token.text[0].islower() and not next_token.is_punct:
                     pass
                 elif next_token.text not in self._quote_or_bracket:
                     next_token.is_sent_start = True
+                elif next_token.text in self._quote_or_bracket \
+                        and has_next2 and next2_token.text[0].isupper() and len(next_token.whitespace_) > 0:
+                    next2_token.is_sent_start = True
                 elif len(token.whitespace_) > 0:
                     next_token.is_sent_start = True
+
         return doc
--- test/test_sentencizer.py
+++ test/test_sentencizer.py
@@ -24,9 +24,14 @@ def nlp():
     ["Szia!", "\"Hogy vagy?\""],
     ["(Szia!) - mondta.", "Hogy vagy?"],
     ["Szinte hihetetlen sebességgel robog az idő.", "A nemrég"],
-    ["Szia!", "Jó estét, Magyarország! című műsor."]
+    ["Szia!", "Jó estét, Magyarország! című műsor."],
+    ["A most megjelent Sport '99 évkönyv címlapjának legnagyobb képén ön feszít ...", "— Hát ez egy külön sztori!"],
+    # ["Vajon önmagában sikernek tekinthető -e? — tette fel a kérdést a napokban a térség befolyásos gazdasági"],
+    ["Minden eszközzel megpróbálnak lejáratni. \"", "Helmeczy szerint ez az oka annak"],
+    ["a közoktatásban dolgozók keresetének felzárkóztatása.", "\"Nem csináltam semmit"],
+    ["A jövőben szigorúan betartják a termelési kvótákat.)", "Egyes hazai szakértők szerint"]
 ])
 def test_sentencizer(nlp: Language, sentences: List[str]):
     doc: Doc = nlp(" ".join(sentences))
-    actual: List[str] = [str(sent) for sent in doc.sents]
+    actual: List[str] = [str(sent).strip() for sent in doc.sents]
     assert sentences == actual
