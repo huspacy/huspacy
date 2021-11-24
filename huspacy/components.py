ae41dc1e69cbf783ae866b96047a7725556136d2
Gyorgy Orosz <gyorgy@orosz.link>
2021-11-24 12:56:22 +0100

    Fixed lemmatizer integration

--- huspacy/components.py
+++ huspacy/components.py
@@ -1,9 +1,63 @@
 import re
-from typing import Optional
+from pathlib import Path
+from typing import Optional, Callable
 
+from spacy.lang.hu import Hungarian
 from spacy.language import Language
+from spacy.pipeline.lemmatizer import lemmatizer_score
 from spacy.tokens import Token
 from spacy.tokens.doc import Doc
+# noinspection PyUnresolvedReferences
+from spacy.training.example import Example
+from spacy.util import ensure_path
+
+from lemmy import Lemmatizer
+
+
+# noinspection PyUnusedLocal
+@Hungarian.factory(
+    "lemmatizer",
+    assigns=["token.lemma"],
+    requires=["token.pos"],
+    default_config={
+        "model_path": None,
+        "scorer": {"@scorers": "spacy.lemmatizer_scorer.v1"},
+    },
+    # default_score_weights={"lemma_acc": 1.0},
+)
+def make_lemmatizer(nlp: Language, name: str, model_path: Path, scorer: Optional[Callable] ) -> "HunLemmatizer":
+    # FIXME: this is a warkaround, `assemble` always passes the configured model_path,
+    #  which becomes invalid when the model is assembled / packaged
+    model = None
+    if model_path.exists():
+        model = Lemmatizer.from_disk(model_path)
+
+    return HunLemmatizer(model, scorer)
+
+
+class HunLemmatizer:
+
+    def __init__(self, model: Lemmatizer, scorer: Optional[Callable] = lemmatizer_score):
+        self._lemmy: Lemmatizer = model
+        self.scorer = scorer
+
+    def __call__(self, doc: Doc) -> Doc:
+        token: Token
+        for token in doc:
+            token.lemma_ = self._lemmy.lemmatize(token.tag_, token.text, token.is_sent_start)
+        return doc
+
+    # noinspection PyUnusedLocal
+    def to_disk(self, path, exclude=tuple()):
+        path: Path = ensure_path(path)
+        path.mkdir(exist_ok=True)
+        self._lemmy.to_disk(path / "model")
+
+    # noinspection PyUnusedLocal
+    def from_disk(self, path, exclude=tuple()) -> "HunLemmatizer":
+        path: Path = ensure_path(path)
+        self._lemmy = Lemmatizer.from_disk(path / "model")
+        return self
 
 
 class HunSentencizer:
