bc286b76853afaa2a0a3e376dc6c788eaeb71167
a100 <a100@a100-gpu-01>
2022-03-09 11:20:05 +0000

    Improved preprocess script for Webcorpus2

--- scripts/process_webcorpus2.sh
+++ scripts/process_webcorpus2.sh
@@ -1,2 +1,13 @@
-wget -q -O - https://nessie.ilab.sztaki.hu/~ndavid/Webcorpus2_clean/$1 | zcat | grep "# text =" | cut -c 10- > ../data/processed/webcorpus2/cleaned/${1::-7}.txt
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+PREPROCESS_SCRIPT=$SCRIPT_DIR/../tools/cli/preprocess_webcorpus.py
+OUT_DIR=$2
+
+wget -q -O - https://nessie.ilab.sztaki.hu/~ndavid/Webcorpus2_clean/$1 \
+    | zcat \
+    | grep "# text =" \
+    | cut -c 10- \
+    | $PREPROCESS_SCRIPT \
+    > $OUT_DIR/${1::-7}.txt
 echo "$1 ✔️"
