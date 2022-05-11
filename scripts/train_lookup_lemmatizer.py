a4be0bedbd3fd8438e1065fd9c421718e710d21d
Peter Berkecz <berkecz.peter@gmail.com>
2022-05-11 13:40:47 +0200

    feat(huspacy): Added lookup lemmatizer
    Lookup Lemmatizer added, which uses spaCy's Lookup and Tables, it returns a lemma based on the token and it's POS tag

---
+++ scripts/train_lookup_lemmatizer.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import typer
+
+from tqdm import tqdm
+from conllu import parse_incr
+
+from spacy.lookups import Lookups, Table
+
+app = typer.Typer()
+
+
+@app.command()
+def main(input_file: str, output_path: Path, min_occurrences: int = 1):
+    token_lemma_occurrences = dict()
+
+    with open(input_file) as inp:
+        for sentence in tqdm(list(parse_incr(inp))):
+            for token in sentence:
+                if token["upos"] not in token_lemma_occurrences.keys():
+                    token_lemma_occurrences[token["upos"]] = dict()
+                if token["form"] not in token_lemma_occurrences[token["upos"]]:
+                    token_lemma_occurrences[token["upos"]][token["form"]] = {
+                        "lemma": token["lemma"], "occurrences": 1}
+                else:
+                    token_lemma_occurrences[token["upos"]
+                                            ][token["form"]]["occurrences"] += 1
+
+    lookups = Lookups()
+
+    for pos in token_lemma_occurrences:
+        table = Table(name=f"lemma_lookups_{pos}")
+        for token in token_lemma_occurrences[pos]:
+            if token_lemma_occurrences[pos][token]["occurrences"] >= min_occurrences:
+                table[token] = token_lemma_occurrences[pos][token]["lemma"]
+
+        lookups.set_table(name=f"lemma_lookups_{pos}", table=table)
+
+    lookups.to_disk(output_path)
+
+
+if __name__ == "__main__":
+    app()
