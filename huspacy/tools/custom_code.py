da5510e14d3f3ffdf89d7d22d076386117084a27
Peter Berkecz <berkecz.peter@gmail.com>
2022-09-20 07:39:19 +0200

    refactor(huspacy): moved hun_sentencizer into it's own file

--- huspacy/tools/custom_code.py
+++ huspacy/tools/custom_code.py
@@ -4,3 +4,4 @@ sys.path.append("../huspacy/tools")
 import components.lemma_postprocessing
 import components.lookup_lemmatizer
 import components.edit_tree_lemmatizer
+import components.hun_sentencizer
