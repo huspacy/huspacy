96c4981ebd35a5fce7f15baf80428e53d1bc29fd
Peter Berkecz <berkecz.peter@gmail.com>
2022-10-03 13:52:10 +0200

    refactor(huspacy): changed components into class factories

--- huspacy/tools/components/lemma_postprocessing.py
+++ huspacy/tools/components/lemma_postprocessing.py
@@ -1,5 +1,7 @@
 from spacy.lang.hu import Hungarian
-from spacy import Doc
+from spacy.pipeline import Pipe
+from spacy import Language
+from spacy.tokens.doc import Doc
 import re
 
 
@@ -26,42 +28,40 @@ def lemma_case_smoother(doc: Doc) -> Doc:
     return doc
 
 
-@Hungarian.component(
-    "lemma_smoother", assigns=["token.lemma"], requires=["token.lemma", "token.pos"]
-)
-def lemma_smoother(doc: Doc) -> Doc:
-    """Fix lemma by regexes.
+class LemmaSmoother(Pipe):
+    """Smooths lemma by applying rules.
 
     Args:
-        doc (Doc): Input document.
-
-    Returns:
-        Doc: Output document.
+        Pipe (Pipe): spaCy Pipe
     """
-    lemma_pp = LemmaPostprocessor()
-    rules = [
-        lemma_pp.remove_exclamation_marks,
-        lemma_pp.remove_question_marks,
-        lemma_pp.fix_date_suffixes,
-        lemma_pp.remove_suffix_after_numbers,
-    ]
 
-    for token in doc:
-        for rule in rules:
-            modified = rule(token.text, token.lemma_, token.pos_)
-            token.lemma_ = modified if modified != None else token.lemma_
+    @staticmethod
+    @Hungarian.factory(
+        "lemma_smoother", assigns=["token.lemma"], requires=["token.lemma", "token.pos"]
+    )
+    def create_lemma_smoother(nlp: Hungarian, name: str) -> "LemmaSmoother":
+        return LemmaSmoother()
 
-    return doc
+    def __init__(self):
+        self._date_regex = re.compile(r"(\d+)-[j]?[éá]n?a?(t[őó]l)?")
+        self._number_regex = re.compile(r"(\d+([-,/_.:]?(._)?\d+)*[%]?(_km/h)?)")
 
+    def __call__(self, doc: Doc) -> Doc:
+        rules = [
+            self.remove_exclamation_marks,
+            self.remove_question_marks,
+            self.fix_date_suffixes,
+            self.remove_suffix_after_numbers,
+        ]
 
-class LemmaPostprocessor:
-    """Class for lemma postprocessing rules."""
+        for token in doc:
+            for rule in rules:
+                modified = rule(token.text, token.lemma_, token.pos_)
+                token.lemma_ = modified if modified != None else token.lemma_
 
-    DATE_REGEX = re.compile(r"(\d+)-[j]?[éá]n?a?(t[őó]l)?")
-    NUMBER_REGEX = re.compile(r"(\d+([-,/_.:]?(._)?\d+)*[%]?(_km/h)?)")
+        return doc
 
-    @classmethod
-    def remove_exclamation_marks(cls, token: str, lemma: str, pos: str) -> str:
+    def remove_exclamation_marks(self, token: str, lemma: str, pos: str) -> str:
         """Removes exclamation marks from the lemma.
 
         Args:
@@ -76,8 +76,7 @@ class LemmaPostprocessor:
         if "!" != lemma and "!" in lemma:
             return lemma.split("!")[0]
 
-    @classmethod
-    def remove_question_marks(cls, token: str, lemma: str, pos: str) -> str:
+    def remove_question_marks(self, token: str, lemma: str, pos: str) -> str:
         """Removes question marks from the lemma.
 
         Args:
@@ -92,8 +91,7 @@ class LemmaPostprocessor:
         if "?" != lemma and "?" in lemma:
             return lemma.split("?")[0]
 
-    @classmethod
-    def fix_date_suffixes(cls, token: str, lemma: str, pos: str) -> str:
+    def fix_date_suffixes(self, token: str, lemma: str, pos: str) -> str:
         """Fixes the suffixes of dates.
 
         Args:
@@ -105,11 +103,10 @@ class LemmaPostprocessor:
             str: The modified lemma.
         """
 
-        if pos == "NOUN" and re.match(cls.DATE_REGEX, lemma):
-            return re.search(cls.DATE_REGEX, lemma).group(0) + "."
+        if pos == "NOUN" and re.match(self._date_regex, lemma):
+            return re.search(self._date_regex, lemma).group(0) + "."
 
-    @classmethod
-    def remove_suffix_after_numbers(cls, token: str, lemma: str, pos: str) -> str:
+    def remove_suffix_after_numbers(self, token: str, lemma: str, pos: str) -> str:
         """Removes suffixes after numbers.
 
         Args:
@@ -121,47 +118,55 @@ class LemmaPostprocessor:
             str: The modified lemma.
         """
 
-        if pos == "NUM" and re.match(cls.NUMBER_REGEX, token):
-            return re.search(cls.NUMBER_REGEX, token).group(0)
+        if pos == "NUM" and re.match(self._number_regex, token):
+            return re.search(self._number_regex, token).group(0)
 
 
-@Hungarian.component(
-    "roman_to_arabic", assigns=["token.lemma"], requires=["token.text"]
-)
-def roman_to_arabic(doc: Doc) -> Doc:
+class RomanToArabic(Pipe):
     """Converts roman numerals to arabic numerals.
 
     Args:
-        doc (Doc): The document to process.
+        Pipe (Pipe): spaCy Pipe
     """
-    r = re.compile(r"^([IVXLCDM]+(-[IVXLCDM]+)*)[.]?$")
 
-    for token in doc:
-        if token.pos_ == "ADJ" and re.match(r, token.text):
-            roman = re.search(r, token.text).group(0)
-            romans = roman.split("-")
-            values = []
+    @staticmethod
+    @Language.factory(
+        "roman_to_arabic", assigns=["token.lemma"], requires=["token.text"]
+    )
+    def create_component(nlp: Language, name: str):
+        return RomanToArabic()
 
-            for r in romans:
-                dot = "." if "." == r[-1] else ""
-                r = r[:-1] if dot else r
+    def __init__(self):
+        self._regex = re.compile(r"^([IVXLCDM]+(-[IVXLCDM]+)*)[.]?$")
+        self._rom_val = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
 
-                rom_val = {
-                    "I": 1,
-                    "V": 5,
-                    "X": 10,
-                    "L": 50,
-                    "C": 100,
-                    "D": 500,
-                    "M": 1000,
-                }
-                int_val = 0
-                for i in range(len(r)):
-                    if i > 0 and rom_val[r[i]] > rom_val[r[i - 1]]:
-                        int_val += rom_val[r[i]] - 2 * rom_val[r[i - 1]]
-                    else:
-                        int_val += rom_val[r[i]]
+    def __call__(self, doc: Doc) -> Doc:
+        for token in doc:
+            if token.pos_ == "ADJ" and re.match(self._regex, token.text):
+                roman = re.search(self._regex, token.text).group(0)
+                romans = roman.split("-")
+                values = []
 
-                values.append(str(int_val) + dot)
+                for r in romans:
+                    dot = "." if "." == r[-1] else ""
+                    r = r[:-1] if dot else r
 
-            token.lemma_ = "-".join(values)
+                    int_val = 0
+                    for i in range(len(r)):
+                        if i > 0 and self._rom_val[r[i]] > self._rom_val[r[i - 1]]:
+                            int_val += self._rom_val[r[i]] - 2 * self._rom_val[r[i - 1]]
+                        else:
+                            int_val += self._rom_val[r[i]]
+
+                    values.append(str(int_val) + dot)
+
+                token.lemma_ = "-".join(values)
+        return doc
