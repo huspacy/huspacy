8952ca882bdc202aafe7bc7f4219f911f4e8efc9
Gyorgy Orosz <gyorgy@orosz.link>
2023-05-22 12:54:58 +0000

    fix: https://github.com/huspacy/huspacy/issues/54

--- scripts/smoke_test.py
+++ scripts/smoke_test.py
@@ -1,4 +1,5 @@
 import traceback
+import random
 
 import spacy
 
@@ -14,13 +15,23 @@ except ImportError as e:
     print("Could not load the components")
 
 
+def get_random_text(nlp, length: int) -> str:
+    words = [lexeme.text for lexeme in nlp.vocab]
+    random.seed(42)
+    chosen_words = random.choices(words, k=length)
+    return " ".join(chosen_words)
+
+
 @app.command()
-def main(path: str):
+def main(path: str, with_random_text: bool = False):
     try:
         nlp = spacy.load(path)
         nlp.add_pipe("conll_formatter")
         doc = nlp("Csiribiri csiribiri Zabszalma - Négy csillag közt alszom ma.")
         print(doc._.conll_str)
+        if with_random_text is not None:
+            doc = nlp(get_random_text(nlp, 1024))
+            print(doc._.conll_str)
     except Exception:
         print(traceback.format_exc())
         exit(1)
