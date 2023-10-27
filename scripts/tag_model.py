860cce97c61e28c9c0112d0004a7c0e29ff3d62e
Gyorgy Orosz <gyorgy@orosz.link>
2023-10-27 18:00:38 +0000

    build: improved model tagging script

--- scripts/tag_model.py
+++ scripts/tag_model.py
@@ -8,10 +8,14 @@ app = Typer()
 
 
 @app.command()
-def main(repo_name: str, tag: str, message: Optional[str] = typer.Option(None, "-m", help="Tag message"),
-         organization=typer.Option("huspacy", "-o", help="Organization slug")):
+def main(repo_name: str, tag: str,
+         message: Optional[str] = typer.Option(None, "-m", help="Tag message"),
+         organization: str=typer.Option("huspacy", "-o", help="Organization slug"),
+         delete_existing: bool = typer.Option(False, "-d", help="Delete tag if exists")):
     # token = typer.prompt("Auth token")
     api = HfApi()
+    if delete_existing:
+        api.delete_tag(repo_id=f"{organization}/{repo_name}", tag=tag)
     api.create_tag(repo_id=f"{organization}/{repo_name}", tag=tag, tag_message=message)
 
 
