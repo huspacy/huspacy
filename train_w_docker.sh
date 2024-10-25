b2a72f21e8301225a32c892a3c095ace9db08a0e
György Orosz <gyorgy@orosz.link>
2024-10-25 13:13:25 +0200

    build: updated docker trainer script allowing to run multiple instances

--- train_w_docker.sh
+++ train_w_docker.sh
@@ -13,15 +13,14 @@ fi
 
 model_dir=$1
 
-docker buildx build -t trainer "${model_dir}" --build-context root="." -f ./Dockerfile
-docker run -it --rm --name trainer \
+docker buildx build -t trainer-${model_dir} "${model_dir}" --build-context root="." -f ./Dockerfile
+docker run -it --rm --name trainer-${model_dir} \
   --runtime=nvidia \
   -v "$(pwd)/${model_dir}"/data:/app/model/data \
   -v "$(pwd)/${model_dir}"/models:/app/model/models \
   -v "$(pwd)/${model_dir}"/configs:/app/model/configs \
   -v "$(pwd)/${model_dir}"/packages:/app/model/packages \
   -v "$(pwd)/${model_dir}"/wandb:/app/model/wandb \
-  -v "$(pwd)/${model_dir}"/project.lock:/app/model/project.lock \
   -v "$(pwd)/${model_dir}"/../huspacy:/app/huspacy \
   -v "$(pwd)/${model_dir}"/../scripts:/app/scripts \
-  trainer "./train.sh"
+  trainer-${model_dir} "./train.sh"
