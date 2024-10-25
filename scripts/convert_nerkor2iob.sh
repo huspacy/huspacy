a24f6f54df88245af0f0e07c7d8d713efa8146c8
György Orosz <gyorgy@orosz.link>
2024-10-25 12:45:40 +0200

    fix(scripts): converter bash script failed

--- scripts/convert_nerkor2iob.sh
+++ scripts/convert_nerkor2iob.sh
@@ -2,7 +2,6 @@
 
 pattern=$1
 dir=${pattern%%/*}
-find $dir -wholename "$pattern" -print \
-  | xargs cat -s \
+find $dir -wholename "$pattern" -print | xargs cat -s \
   | awk -F $'\t' 'BEGIN {id=0; OFS=FS} NF==0 {id=0; print} NF>0 && substr($1, 1, 1) != "#" {id=id+1; print $1,$NF}' \
   > "$2"
